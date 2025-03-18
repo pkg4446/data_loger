aba1123bdde9ff071aa3834d2dca57e95331645f
pkg4446 <pkg4446@gmail.com>
2025-03-18 16:38:48 +0900

    250318

--- daesung_v1/routes/index.js
+++ daesung_v1/routes/index.js
@@ -2,10 +2,10 @@ const express   = require('express');
 const router    = express.Router();
 
 const web       = require('./web');
-const device    = require('./device');
+const log       = require('./log');
 const admin     = require('./admin');
 const user      = require('./user');
-const reqest    = require('./reqest');
+const device    = require('./device');
 
 router.route("/")
     .get(async function(req, res) {
@@ -17,9 +17,9 @@ router.route("/")
         res.status(201).send(response);
     });
 router.use('/web',web);
-router.use('/device',device);
+router.use('/log',log);
 router.use('/admin',admin);
 router.use('/user',user);
-router.use('/reqest',reqest);
+router.use('/device',device);
 
 module.exports  = router;
