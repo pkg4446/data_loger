7447947a269d5f31e89d8257ea3ebde3fa22e628
pkg4446 <pkg4446@gmail.com>
2025-04-03 13:05:02 +0900

    250403

--- daesung_v1/routes/device.js
+++ daesung_v1/routes/device.js
@@ -93,16 +93,12 @@ router.post('/hub', async function(req, res) {
             }
         }else if(req.body.API == "set"){
             console.log(req.body);
-            if(await file_system.check(path_device + "/config.json")){
-                const device_config   = JSON.parse(await file_system.fileRead(path_device,"config.json"));
-                device_config.ex_goal = req.body.DATA.goal;
-                device_config.goal    = req.body.DATA.goal;
-                device_config.ex_run  = req.body.DATA.run;
-                device_config.run     = req.body.DATA.run;
-                await file_system.fileMK(path_device,JSON.stringify(device_config),"config.json");
-            }else{
-                await file_system.fileMK(path_device,JSON.stringify(req.body.DATA),"config.json");
-            }
+            const device_config   = JSON.parse(await file_system.fileRead(path_device,"config.json"));
+            device_config.ex_goal = req.body.DATA.goal;
+            device_config.goal    = req.body.DATA.goal;
+            device_config.ex_run  = req.body.DATA.run;
+            device_config.run     = req.body.DATA.run;
+            await file_system.fileMK(path_device,JSON.stringify(device_config),"config.json");
         }
     }
     res.status(status).send(response);
