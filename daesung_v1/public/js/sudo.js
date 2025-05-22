d2ca1f0257bbfc2702984f62618ba8a07c2026c1
pkg4446 <pkg4446@gmail.com>
2025-05-22 13:03:25 +0900

    250522

--- daesung_v1/public/js/sudo.js
+++ daesung_v1/public/js/sudo.js
@@ -23,7 +23,7 @@ async function admin_check() {
     else{dashboard();}
 }
 ////--------------------------------------------------------------------////
-function alert_swal(icon,title) {
+async function alert_swal(icon,title) {
     Swal.fire({
         position: "top",
         icon:   icon,
@@ -48,116 +48,107 @@ function ascii() {
 ////-------------------////
 function user_login(user) {
     if(user == localStorage.getItem('user')){
-        alert_swal("info",user + " : Checked");
+        alert_swal("info",user + "로 로그인 중입니다.");
     }else{
         Swal.fire({
             position: "top",
             icon:   "question",
-            title:  user+" 체크",
+            title:  user+"로 로그인 하시겠습니까?",
             showCancelButton: true,
             confirmButtonText: "확인",
             cancelButtonText:  "취소"
-        }).then((result)=>{
+        }).then(async(result)=>{
             if(result.isConfirmed){
-                fetch(window.location.protocol+"//"+window.location.host+"/admin/superuser", {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        token:  localStorage.getItem('manager'),
-                        user:   user
-                    })
-                }).then(response => {
-                    if (response.status==400) {
-                        throw new Error('관리자 TOKEN이 누락됐습니다.');
-                    }else if (response.status==403) {
-                        throw new Error('TOKEN이 유효하지 않습니다.');
-                    }
-                    return response.text(); // JSON 대신 텍스트로 응답을 읽습니다.
-                })
-                .then(data => {
-                    if (data != "fail") {
+                const respose = await fetchData("admin/user_login",{token:localStorage.getItem('manager'),user:user});
+                if (respose.status==400 || respose.status==403) {
+                    alert_swal("error","관리자 접속 오류가 발생했습니다.").then(()=>{admin_logout();});
+                }else if (respose.status==200) {
+                    const data = await respose.text();
+                    if (data != "null") {
                         localStorage.setItem('user', user);
                         localStorage.setItem('token', data);
-                        Swal.fire({
-                            position: "top",
-                            icon:   "success",
-                            title:  user + ": Checked",
-                            showConfirmButton: false,
-                            timer:  1500
-                        });
-                    } else {}
-                })
-                .catch((error) => {
-                    console.error('Error:', error);
-                    Swal.fire({
-                        position: "top",
-                        icon:   "error",
-                        title:  '관리자 접속 오류가 발생했습니다.',
-                        text:   error,
-                        showConfirmButton: false,
-                        timer:  1500
-                    }).then(() => {
-                        admin_authority();
-                    });
-                });
+                        alert_swal("success",user + "로 로그인 되었습니다.");
+                    } else {
+                        alert_swal("warning",user + "의 키가 없습니다.");
+                    }
+                }
             }
         });
     }
 }
 ////-------------------////
-function dashboard() {
+async function dashboard() {
     HTML_scrpit = `<table class="data-table"><thead><tr>
-    <th>ID</th><th>이름</th><th>농장</th><th>주소</th><th>전화</th><th>가입날짜</th>
+    <th>ID</th><th>이름</th><th>농장</th><th>주소</th><th>전화</th>
     </tr></thead><tbody>`;
-    const user_list = [];
+    const user_list = await (await fetchData("admin/user_list",{token:localStorage.getItem('manager')})).json();
+
     for (const user_id in user_list) {
-        const user_info = user_list[user_id];
-        const user_date = new Date(user_info.date);
+        const user_info = JSON.parse(user_list[user_id]);
         HTML_scrpit += `<tr onclick=user_login("${user_id}") style="cursor:pointer;">
             <td>${user_id}</td>
             <td>${user_info.name}</td>
             <td>${user_info.farm}</td>
             <td>${user_info.addr}</td>
             <td>${user_info.tel}</td>
-            <td>${user_date.toISOString().substring(0, 10)}</td>
         </tr>`;
     }
     HTML_scrpit += "</tbody></table>"
     document.getElementById("user_table").innerHTML = HTML_scrpit;
 
     HTML_scrpit = `<table class="data-table"><thead><tr>
-    <th>IP</th><th>ID</th><th>firmware</th><th>등록 유저</th></tr></thead><tbody>`;
-    const device_list = [];
-    for (const device_ip in device_list) {
-        if(device_ip != "ver"){
+    <th>종류</th><th>IP</th><th>장비ID</th><th>등록 유저</th></tr></thead><tbody>`;
+
+    // {
+    //     "act": {
+    //         "::1": {
+    //             "2c_cf_67_47_6d_a1": "test",
+    //             "2c_cf_67_48_45_93": null
+    //         }
+    //     },
+    //     "hive": {
+    //         "::ffff:192.168.1.67": {
+    //             "AC_15_18_CD_20_D0": "test"
+    //         }
+    //     },
+    //     "hub": {
+    //         "::1": {
+    //             "C4_D8_D5_AB_CD_58": "test"
+    //         }
+    //     }
+    // }
+
+    const device_list = await (await fetchData("admin/device_list",{token:localStorage.getItem('manager')})).json();
+    console.log(device_list);
+
+    for (const device_type in device_list) {
+        const type_list = device_list[device_type];
+        let type_once = true;
+        for (const device_ip in type_list) {
+            const ip_list = type_list[device_ip];
             let ip_once = true;
-            for (const device_id in device_list[device_ip]) {
-                let version = device_list[device_ip][device_id].VER;
-                let version_update = true;
-                let user_id = device_list[device_ip][device_id].USER;
-                if(version == null){version = "?.?.?";}
-                else if(version == device_list.ver){
-                    version_update = false;
-                    version = "latest ver";
-                }
+            for (const device in ip_list) {
+                const owner = ip_list[device];
+                console.log(device,owner);
                 HTML_scrpit += "<tr>"
+                if(type_once){
+                    type_once = false;
+                    HTML_scrpit += `<td>${device_type}</td>`;
+                }else{
+                    HTML_scrpit += `<td></td>`;
+                }
                 if(ip_once){
                     ip_once = false;
                     HTML_scrpit += `<td>${device_ip}</td>`;
                 }else{
                     HTML_scrpit += `<td></td>`;
                 }
-                HTML_scrpit += `<td>${device_id}</td><td`;
-                if(version_update){HTML_scrpit += ` onclick=firmware_update("${device_id}") style="cursor:pointer;"`;}
-                HTML_scrpit += `>${version}</td>`;
-                if(user_id == null){HTML_scrpit  += `<td onclick=device_regist("${device_ip}","${device_id}")`;}
-                else{HTML_scrpit += `<td onclick=device_del("${device_ip}","${device_id}","${user_id}")`;}
-                HTML_scrpit += ` style="cursor:pointer;">${user_id}</td></tr>`;
+                HTML_scrpit += `<td>${device}</td>`;
+                HTML_scrpit += `<td>${owner}</td>`;
             }
         }
     }
+
     HTML_scrpit += "</tbody></table>"
     document.getElementById("device_table").innerHTML = HTML_scrpit;
 }
